103268149f79a3a6e5b905efbe92c916d2e5d319
kingraquel <107775696+kingraquel@users.noreply.github.com>
2022-06-26 13:23:24 -1000

    Add files via upload

---
+++ treasurehunt.js
@@ -0,0 +1,27 @@
+const treasure = (location) => {
+  if(location === 0) {
+    console.log("location is 0")
+    document.getElementById(location).innerHTML = "🌿"
+  } else if(location === 1) {
+    console.log("location is 1")
+    document.getElementById(location).innerHTML = "🌿"
+  } else if(location === 2) {
+    console.log("location is 2")
+    document.getElementById(location).innerHTML = "🌿"
+  } else if(location === 3) {
+    console.log("location is 3")
+    document.getElementById(location).innerHTML = "🦋"
+  } else if(location === 4) {
+    console.log("location is 4")
+    document.getElementById(location).innerHTML = "🌿"
+  } else if(location === 5) {
+    console.log("location is 5")
+    document.getElementById(location).innerHTML = "🕷"
+  } else if(location === 6) {
+    console.log("location is 6")
+    document.getElementById(location).innerHTML = "🌿"
+  } else if(location === 7) {
+    console.log("location is 7")
+    document.getElementById(location).innerHTML = "🌿"
+  }
+}
